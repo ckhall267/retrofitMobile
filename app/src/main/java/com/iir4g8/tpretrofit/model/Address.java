e3c0c482f611e67704ca94a21e430e1ac445a61e
ckhall267 <chaima.khalil@emsi-edu.ma>
2025-05-23 17:06:53 +0100

    Ajout du projet TPRetrofit

---
+++ app/src/main/java/com/iir4g8/tpretrofit/model/Address.java
@@ -0,0 +1,14 @@
+package com.iir4g8.tpretrofit.model;
+
+public class Address {
+    private String city;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+}
---
+++ app/src/main/java/com/iir4g8/tpretrofit/model/User.java
@@ -0,0 +1,16 @@
+package com.iir4g8.tpretrofit.model;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private Address address;
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getPhone() { return phone; }
+    public Address getAddress() { return address; }
+
+}
